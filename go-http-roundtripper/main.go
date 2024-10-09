9561008d5c0a67213171a3f75b571fcb1bda2983
joechea <s.chea@aupp.edu.kh>
2024-10-09 09:12:23 +0700

    add http roundtripper to go example - add auth example

--- go-http-roundtripper/main.go
+++ go-http-roundtripper/main.go
@@ -9,27 +9,39 @@ import (
 )
 
 func main() {
-	c := &http.Client{
-		// Transport: &loggingRoundTripper{
-		// 	next: http.DefaultTransport,
-		// 	logger:              os.Stdout,
-		// },
-		Transport: &retryRoundTripper{
-			next: &loggingRoundTripper{
-				next:   http.DefaultTransport,
-				logger: os.Stdout,
+	// c := &http.Client{
+	// 	Transport: &retryRoundTripper{
+	// 		next: &loggingRoundTripper{
+	// 			next:   http.DefaultTransport,
+	// 			logger: os.Stdout,
+	// 		},
+	// 		maxRetries: 3,
+	// 		delay:      time.Duration(1 * time.Second),
+	// 	},
+	// }
+
+	authedClient := &http.Client{
+		Transport: &authRoundTripper{
+			next: &retryRoundTripper{
+				next: &loggingRoundTripper{
+					next:   http.DefaultTransport,
+					logger: os.Stdout,
+				},
+				maxRetries: 3,
+				delay:      time.Duration(1 * time.Second),
 			},
-			maxRetries: 3,
-			delay:      time.Duration(1 * time.Second),
+			user: "bob",
+			pwd:  "pwd",
 		},
 	}
 	// req, err := http.NewRequest(http.MethodGet, "http://httpbin.org/get", nil)
-	req, err := http.NewRequest(http.MethodGet, "http://httpbin.org/status/500", nil)
+	// req, err := http.NewRequest(http.MethodGet, "http://httpbin.org/status/500", nil)
+	req, err := http.NewRequest(http.MethodGet, "http://httpbin.org/basic-auth/bob/pwd", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	res, err := c.Do(req)
+	res, err := authedClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
@@ -43,6 +55,16 @@ func main() {
 	fmt.Println("\n---Response---")
 	fmt.Println("STATUS CODE: ", res.StatusCode)
 	fmt.Println("BODY: ", string(body))
+}
+
+type authRoundTripper struct {
+	next      http.RoundTripper
+	user, pwd string
+}
+
+func (a authRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.SetBasicAuth(a.user, a.pwd)
+	return a.next.RoundTrip(r)
 }
 
 type loggingRoundTripper struct {
